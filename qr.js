2154db9c46b2946f591faf2e77d6291b3f679bb7
Alejandro_Camacho <alejandro.camacho.dev.01@gmail.com>
2025-05-23 11:42:10 -0400

    manage ip

--- qr.js
+++ qr.js
@@ -6,8 +6,7 @@ let tokens = [];
 
 const PORT = process.env.PORT || 3001;
 
-const SERVER_URL = 'http://192.168.104.217:3001/api/token';
-// const SERVER_URL = 'http://dashboard.pasantia.com/api/token';
+const SERVER_URL = 'http://192.168.224.20:3001/api/token';
 console.log(`Iniciando servicio en puerto: ${PORT}`);
 console.log('Tokens almacenados:', tokens);
 const client = new Client({
